a1ed1e49dd183fd5bc7cd8c8e2481acf02351ef6
amo02008 <adrienne.osorio@gmail.com>
2020-11-21 19:33:54 -0500

    added background image and some color to fonts

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # quiz-time
+
+#User Story
+
+I was tasked with creating a timed quiz.  The idea was to present each question one at a time, keep score and to subtract time for wrong answers.  At the end you should be able to save your score and initials.
+
+
+#What I did
+
+What I ended up with was a quiz created using forms. I initially tired to create each question within a bootstrap grid to allow for each question to appear on its own.  As I was not successful at utilizing the bootstrap in this way I changed the quiz template to form to allow for a version of a quiz to still be rendered.  Initially my quiz did have a spot to show your score and save your initials. However, once I added in JS to account for starting the quiz it didn’t not function as needed.  I decided to put some effort in to how the quick looked, aesthetically speaking, since I didn’t meet all the criteria outlined for me.
+
+I am still learning coding and JavaScript has proven to be a tough mountain to climb. 
--- script.js
+++ script.js
@@ -1,26 +1,66 @@
-function check () {
+start.addEventListener("click", startQuiz);
 
-    var q1 = document.quiz.question1.value;
-    var q2 = document.quiz.question1.value;
-    var q3 = document.quiz.question1.value;
-    var correct = 0;
+function startQuiz() {
+    document.getElementById("start").style.visibility = "hidden";
+    document.getElementById("quiz").style.visibility = "visible";
+    document.getElementById("after_submit").style.visibility = "visible";
+    document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";   
 
-    if (question1 == "Brendan Eich") {
-        correct++;
+
+    function check() {
+
+        var question1 = document.quiz.question1.value;
+        var question2 = document.quiz.question2.value;
+        var question3 = document.quiz.question3.value;
+        var question4 = document.quiz.question4.value;
+        var correct = 0;
+
+        if (question1 == "Brendan Eich") {
+            correct++;
+        }
+        
+
+        if (question2 == "ESLint") {
+            correct++;
+        }
+
+        if (question3 == "just-in-time") {
+            correct++;
+        }
+
+        if (question4 == "Float") {
+            correct++;
+        }
+       
+        
+
+
     }
+}
+after_submit.addEventListener("click", button);
+function myFunction () {
+document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
+}
 
-    if (question2 == "ESLint") {
-        correct++;
-    }
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
 
-    if (question3 == "just-in-time") {
-        correct++;
-    }
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
 
-    if (question4 == "Float") {
-        correct++;
-    }
- document.getElementById("after_submit").style.visibility = "visible";
- document.getElementById("number_correct").innerHTML = "You got" + correct + "correct.";
+        display.textContent = minutes + ":" + seconds;
 
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var twoMinutes = 60 * 2,
+        display = document.querySelector('#time');
+    startTimer(twoMinutes, display);
 }
